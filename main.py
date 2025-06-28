00de0aab09cb1d4625a69a44d21accb30a53d0a4
jncogs <cogswell.jesse@gmail.com>
2025-06-27 21:02:17 -0500

    updated imports in main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 import sys
-import subprocess
+
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.run_python import run_python_file
+from functions.write_file import write_file
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
